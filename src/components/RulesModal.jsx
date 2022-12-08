7bbf307e80c8996535f2b6cc09343f3363c0d8fd
Helena Heil <helenahcodes@gmail.com>
2022-12-08 13:15:05 +0100

    simplify Rules modal

--- src/components/RulesModal.jsx
+++ src/components/RulesModal.jsx
@@ -1,17 +1,12 @@
 import styled from "styled-components";
 import CloseIcon from "../assets/img/icon-close.svg";
-import { OPTIONS_GRAY } from "../utils";
-import OptionButton from "./OptionButton";
+import RulesImg from "../assets/img/image-rules.svg";
 
 export default function RulesModal({ onClose }) {
   return (
     <ModalWrapper>
       <h2>RULES</h2>
-      <OptionWrapper>
-        {OPTIONS_GRAY.map((option) => (
-          <OptionButton key={option.name} option={option} />
-        ))}
-      </OptionWrapper>
+      <img src={RulesImg} alt="Rules" />
       <button onClick={() => onClose(false)}>
         <img src={CloseIcon} alt="Close Modal" />
       </button>
@@ -33,10 +28,4 @@ const ModalWrapper = styled.div`
   background: #fff;
   color: hsl(229, 25%, 31%);
   z-index: 100;
-`;
-
-const OptionWrapper = styled.div`
-  position: relative;
-  width: 100%;
-  top: -100px;
 `;
